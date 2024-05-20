f37660363ab4ef543f41160b3ed283adcbbb4dce
Bryan Pritchett <dasassygoat@coderbox.co>
2024-05-19 23:20:02 -0500

    qodana

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,16 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField]private float steerSpeed = 0.1f;
     [SerializeField]private float moveSpeed = 0.01f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()

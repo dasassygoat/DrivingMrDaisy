d44263fc1e50f3987f3f7df43af009d44c50fdff
Bryan Pritchett <dasassygoat@coderbox.co>
2024-06-05 00:06:47 -0500

    car color changed

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] private Color32 hasPackageColor = new Color32(255, 0, 0, 255);
+    [SerializeField] private Color32 noPackageColor = new Color32(255, 0, 0, 255);
     [SerializeField]
     float destroyTime = 0.5f;
     private bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Collided with " + other.gameObject.name);
@@ -19,12 +28,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
         }
 
         if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Delivered Package");
             Destroy(other.gameObject, destroyTime);
+            spriteRenderer.color = noPackageColor;
         }
         else if (other.tag == "Customer" && !hasPackage)
         {

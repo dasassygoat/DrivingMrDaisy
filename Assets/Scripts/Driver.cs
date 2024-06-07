08a0a0242dc9915b819aa1fd78252a2911537d6e
Bryan Pritchett <dasassygoat@coderbox.co>
2024-06-06 23:55:10 -0500

    bump and boost implementation

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField]private float steerSpeed = 0.1f;
-    [SerializeField]private float moveSpeed = 0.01f;
-    [SerializeField] private float slowSpeed = 0.005f;
-    [SerializeField] private float bumpSpeed = 0.005f;
+    [SerializeField]private float steerSpeed = 200f;
+    [SerializeField]private float moveSpeed = 20f;
+    private float bumpSpeed = 10f;
+    private float boostSpeed = 50f;
 
     // Update is called once per frame
     private void Update()
@@ -16,6 +16,22 @@ public class Driver : MonoBehaviour
         transform.Rotate(0, 0, steerSpeed * -steerAmount * Time.deltaTime); //rotate the car
 
         transform.Translate(0, moveSpeed * moveAmount * Time.deltaTime, 0); //move the car forward
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bump")
+        {
+            moveSpeed = bumpSpeed;
+            Debug.Log("Bumped");
+            Debug.Log("Speed: " + moveSpeed);
+        }
+        else if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+            Debug.Log("Boost");
+            Debug.Log("Speed: " + moveSpeed);
+        }
     }
 
 
